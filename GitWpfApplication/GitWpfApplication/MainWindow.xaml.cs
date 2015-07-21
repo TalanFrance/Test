ea0864e98874f242ba449d4adf96153e246e99a6
MehdiBATalan <mehdi.benammar@talan.tn>
2015-07-21 14:42:13 +0200

    Ajout de la fct Multiplication

--- GitWpfApplication/GitWpfApplication/MainWindow.xaml.cs
+++ GitWpfApplication/GitWpfApplication/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -12,6 +13,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using GitWpfApplication.Sources;
 
 namespace GitWpfApplication
 {
@@ -20,9 +22,23 @@ namespace GitWpfApplication
     /// </summary>
     public partial class MainWindow : Window
     {
+        CalculatorVM calculator;
         public MainWindow()
         {
+            calculator = new CalculatorVM();
+            this.DataContext = calculator;
             InitializeComponent();
+        }
+
+        private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("^[1-9][0-9]*");
+            e.Handled = !regex.IsMatch(e.Text);
+        }
+
+        private void btnCalculer_Click(object sender, RoutedEventArgs e)
+        {
+            calculator.Multiplier();
         }
     }
 }
---
+++ GitWpfApplication/GitWpfApplication/Sources/CalculatorVM.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ComponentModel;
+
+namespace GitWpfApplication.Sources
+{
+    public class CalculatorVM: INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        private int nombre1;
+        private int nombre2;
+        private int result;
+
+        public int Nombre1 
+        {
+            get
+            {
+                return nombre1;
+            } 
+            set
+            {
+                if (nombre1 != value)
+                {
+                    nombre1 = value;
+                    OnPropertychanged("Nombre1");
+                }
+            }
+        }
+
+        public int Nombre2 
+        { 
+            get
+            {
+                return nombre2;
+            }
+
+            set
+            {
+                if (nombre2 != value)
+                {
+                    nombre2 = value;
+                    OnPropertychanged("Nombre2");
+                }
+            }
+        }
+
+        public int Result 
+        { 
+            get
+            {
+                return result;
+            }
+
+            set
+            {
+                if (result != value)
+                {
+                    result = value;
+                    OnPropertychanged("Result");
+                }
+            }
+        }
+
+        public void Multiplier()
+        {
+            Result = nombre1 * nombre2;
+        }
+
+        protected void OnPropertychanged(string name)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(name));
+            }
+        }
+
+
+    }
+}
